7bf902e0f2221b4fd7ac4962601e50a7945d0d4d
Olagoke Olorunkosebi <olagokeolorunkosebi@mail.com>
2022-09-23 14:16:11 +0200

    Drawing App Complete

--- drawing.js
+++ drawing.js
@@ -1,1 +1,118 @@
-$(function () {});
+$(function () {
+  // declare variables
+  // paintingerasing or not
+  var paint = false;
+  //
+  // painting or erasing
+  var paint_erase = "paint";
+  //
+  // get the canvas and context
+  var canvas = document.getElementById("paint");
+  var ctx = canvas.getContext("2d");
+  //
+  // get the canvas container
+  var container = $("#canvasContainer");
+  //
+  // mouse position
+  var mouse = {
+    x: 0,
+    y: 0,
+  };
+  //
+  // onload load saved work from local storage
+
+  if (localStorage.getItem("imgCanvas") != null) {
+    var img = new Image();
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = localStorage.getItem("imgCanvas");
+  }
+
+  //
+  // set drawing parameters(linewidth, linejoin, linecap)
+  ctx.lineWidth = 3;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
+  //
+  // click inside container
+  container.mousedown(function (e) {
+    paint = true;
+    ctx.beginPath();
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    ctx.moveTo(mouse.x, mouse.y);
+  });
+  //
+  // move the mouse while holding mouse key
+  container.mousemove(function (e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    if (paint == true) {
+      if (paint_erase == "paint") {
+        // get color from color input
+        ctx.strokeStyle = $("#paintColor").val();
+      } else {
+        // white color
+        ctx.strokeStyle = "white";
+      }
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+  });
+  //
+  // mouse up: we are not paintingerasing anymore
+  container.mouseup(function () {
+    paint = false;
+  });
+  //
+  // if we leave the container we are not paintingerasing anymore
+  container.mouseleave(function () {
+    paint = false;
+  });
+  //
+  // click on the reset button
+  $("#reset").click(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    paint_erase = "paint";
+    $("#erase").removeClass("eraseMode");
+  });
+  //
+  // click on the save button
+  $("#save").click(function () {
+    if (typeof localStorage != null) {
+      localStorage.setItem("imgCanvas", canvas.toDataURL());
+    } else {
+      window.alert("Your browser does not support local storage");
+    }
+  });
+
+  //
+  // click on the erase button
+  $("#erase").click(function () {
+    if (paint_erase == "paint") {
+      paint_erase = "erase";
+    } else {
+      paint_erase = "paint";
+    }
+    $(this).toggleClass("eraseMode");
+  });
+  //
+  // change color input
+  $("#paintColor").change(function () {
+    $("#circle").css("background-color", $(this).val());
+  });
+  //
+
+  // slider
+  $("#slider").slider({
+    min: 3,
+    max: 30,
+    slide: function (event, ui) {
+      $("#circle").height(ui.value);
+      $("#circle").width(ui.value);
+      // change line width using a slider
+      ctx.lineWidth = ui.value;
+    },
+  });
+});
